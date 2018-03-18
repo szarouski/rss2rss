0389ff25fcea42962e2c8286cde61064d70f4804
Sergey Zarouski <serge.zarouski@gmail.com>
2018-03-18 14:39:31 -0400

    Filters for habr, css-live, npm

--- conf/default.js
+++ conf/default.js
@@ -6,7 +6,39 @@ module.exports = {
 		source: 'https://habrahabr.ru/rss/feed/posts/all/02c22642248aca2e9dcc8ae9506e8c66/',
 		filters: [{
 			key: 'title',
-			regex: /angular/ig,
+			regex: /\bangular\b/ig,
+		}, {
+			key: 'title',
+			regex: /Дайджест свежих материалов из мира фронтенда за последнюю неделю/ig,
+		}, {
+			key: 'title',
+			regex: /\bVirtualBox\b/ig,
+		}, {
+			key: 'title',
+			regex: /\bFlask\b/ig,
+		}, {
+			key: 'title',
+			regex: /ASP.NET/ig,
+		}, {
+			key: 'title',
+			regex: /\bbootstrap\b/ig,
+		}, {
+			key: 'title',
+			regex: /\bvue\b/ig,
+		}]
+	}, {
+		url: 'npm',
+		source: 'http://blog.npmjs.org/rss',
+		filters: [{
+			key: 'title',
+			regex: /Customer Convo/ig,
+		}]
+	}, {
+		url: 'css-live',
+		source: 'http://css-live.ru/feed',
+		filters: [{
+			key: 'title',
+			regex: /Еженедельная подборка красивых эффектов/ig,
 		}]
 	}]
 };
