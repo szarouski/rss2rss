900f3959d134e6d239d2c6f0e802866cfd2eea61
Sergey Zarouski <serge.zarouski@gmail.com>
2019-04-20 11:06:38 -0400

    Separate habr hubs. Use only weekly top.

--- conf/default.js
+++ conf/default.js
@@ -1,67 +1,117 @@
+const habrFilters = [
+	{
+		key: 'title',
+		regex: /\bangular\b/ig,
+	}, {
+		key: 'title',
+		regex: /Дайджест свежих материалов из мира фронтенда за последнюю неделю/ig,
+	}, {
+		key: 'title',
+		regex: /Полезное дизайнеру/ig,
+	}, {
+		key: 'title',
+		regex: /\bVirtualBox\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bFlask\b/ig,
+	}, {
+		key: 'title',
+		regex: /ASP.NET/ig,
+	}, {
+		key: 'title',
+		regex: /\bphp\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bbootstrap\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bvue\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bjava\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bjoomla\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bpolymer\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bkotlin\b/ig,
+	}, {
+		key: 'title',
+		regex: /\brails\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bwordpress\b/ig,
+	}, {
+		key: 'title',
+		regex: /\bruby\b/ig,
+	}, {
+		key: 'title',
+		regex: /\byii2\b/ig,
+	}, {
+		key: 'title',
+		regex: /Разработка изоморфного RealWorld приложения с SSR и Progressive Enhancement/ig,
+	}
+];
+
 module.exports = {
 	port: process.env.PORT || 80,
 	base: '',
 	feeds: [
 		{
-			url: 'habr',
-			source: 'https://habrahabr.ru/rss/feed/posts/all/02c22642248aca2e9dcc8ae9506e8c66/',
-			filters: [
-				{
-					key: 'title',
-					regex: /\bangular\b/ig,
-				}, {
-					key: 'title',
-					regex: /Дайджест свежих материалов из мира фронтенда за последнюю неделю/ig,
-				}, {
-					key: 'title',
-					regex: /Полезное дизайнеру/ig,
-				}, {
-					key: 'title',
-					regex: /\bVirtualBox\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bFlask\b/ig,
-				}, {
-					key: 'title',
-					regex: /ASP.NET/ig,
-				}, {
-					key: 'title',
-					regex: /\bphp\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bbootstrap\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bvue\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bjava\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bjoomla\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bpolymer\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bkotlin\b/ig,
-				}, {
-					key: 'title',
-					regex: /\brails\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bwordpress\b/ig,
-				}, {
-					key: 'title',
-					regex: /\bruby\b/ig,
-				}, {
-					key: 'title',
-					regex: /\byii2\b/ig,
-				}, {
-					key: 'title',
-					regex: /Разработка изоморфного RealWorld приложения с SSR и Progressive Enhancement/ig,
-				}
-			]
+			url: 'habr-css',
+			source: 'https://habr.com/ru/rss/hub/css/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-webdev',
+			source: 'https://habr.com/ru/rss/hub/webdev/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-chrome',
+			source: 'https://habr.com/ru/rss/hub/google_chrome/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-html',
+			source: 'https://habr.com/ru/rss/hub/html5/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-js',
+			source: 'https://habr.com/ru/rss/hub/javascript/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-k8s',
+			source: 'https://habr.com/ru/rss/hub/kubernetes/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-node',
+			source: 'https://habr.com/ru/rss/hub/nodejs/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-reactjs',
+			source: 'https://habr.com/ru/rss/hub/reactjs/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-safari',
+			source: 'https://habr.com/ru/rss/hub/safari/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-wasm',
+			source: 'https://habr.com/ru/rss/hub/webassembly/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-browsers',
+			source: 'https://habr.com/ru/rss/hub/browsers/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-code',
+			source: 'https://habr.com/ru/rss/hub/complete_code/top/weekly/?fl=ru',
+			filters: habrFilters
+		}, {
+			url: 'habr-a11y',
+			source: 'https://habr.com/ru/rss/hub/accessibility/top/weekly/?fl=ru',
+			filters: habrFilters
 		}, {
 			url: 'npm',
 			source: 'http://blog.npmjs.org/rss',
